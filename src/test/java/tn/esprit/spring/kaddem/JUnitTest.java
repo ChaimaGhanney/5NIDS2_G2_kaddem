15b3efa92dfbb58af35dfacb61247083b48f6458
Hamza BEN MHENNI <hamza.benmhenni@esprit.tn>
2023-11-06 07:46:46 +0100

    modif hamza

--- src/test/java/tn/esprit/spring/kaddem/JUnitTest.java
+++ src/test/java/tn/esprit/spring/kaddem/JUnitTest.java
@@ -14,7 +14,6 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class JUnitTest {
     @Autowired
